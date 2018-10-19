dc11a70e86acdde57de5ff65d8799b7f37dd25d2
mahdi motamedi <mahdi.moatamedi@yahoo.com>
2018-10-19 14:00:01 +0330

    deploy

---
+++ precache-manifest.b3777f9786bbd46a0c1d84f4516c9256.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = [
+  {
+    "revision": "e6ae621e1cac3c725c2d92e79bdd002b",
+    "url": "/fonts/Vazir-Light.e6ae621e.woff"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "9ec22097177b9ff1e0a87793c00cb1d5",
+    "url": "/img/3.9ec22097.jpg"
+  },
+  {
+    "revision": "169456f2d4eecfc8359f",
+    "url": "/css/chunk-vendors.d10d24d6.css"
+  },
+  {
+    "revision": "f70e61de74127345ed80d7646572a1b9",
+    "url": "/img/marry.f70e61de.png"
+  },
+  {
+    "revision": "5b9e5c2dc78c6ee2bd5b31f1056b9c3b",
+    "url": "/img/poker.5b9e5c2d.png"
+  },
+  {
+    "revision": "c6911cf764073c0688afb5b3b0c63019",
+    "url": "/img/5.c6911cf7.jpg"
+  },
+  {
+    "revision": "7a7860bd2468acc9e084f189a176fa3c",
+    "url": "/img/fun-fact.7a7860bd.png"
+  },
+  {
+    "revision": "21b5fbef4ce603d2f8a742451e5f594d",
+    "url": "/img/joke.21b5fbef.png"
+  },
+  {
+    "revision": "92b2acb1b4a5a3d460005222a3dc847e",
+    "url": "/img/devil.92b2acb1.png"
+  },
+  {
+    "revision": "9093f0ee43131b77a7691a83eb7b92a8",
+    "url": "/fonts/Vazir-Medium.9093f0ee.woff"
+  },
+  {
+    "revision": "ff5b4dc141a40d349b8beedb07cc1d64",
+    "url": "/fonts/Vazir-Medium.ff5b4dc1.woff2"
+  },
+  {
+    "revision": "95572690884ef4400477c9d272a10877",
+    "url": "/fonts/Vazir.95572690.woff2"
+  },
+  {
+    "revision": "f7445853c228d0ec8549eead646188ce",
+    "url": "/fonts/Vazir.f7445853.woff"
+  },
+  {
+    "revision": "b509835797d20934db7c6bc07b87fbe1",
+    "url": "/fonts/Vazir-Light.b5098357.woff2"
+  },
+  {
+    "revision": "2cca7d1783a541980ec4a50e74f160ab",
+    "url": "/fonts/Mikhak-Bold.2cca7d17.woff2"
+  },
+  {
+    "revision": "1565d7fe767f9ad481373b05a623c078",
+    "url": "/fonts/Mikhak-Medium.1565d7fe.woff2"
+  },
+  {
+    "revision": "3562d098401b0679d6925d71a59248a4",
+    "url": "/fonts/Mikhak-Light.3562d098.woff2"
+  },
+  {
+    "revision": "d482b3f86bf4ef560d2d",
+    "url": "/js/app.9c284bee.js"
+  },
+  {
+    "revision": "169456f2d4eecfc8359f",
+    "url": "/js/chunk-vendors.779ba5ed.js"
+  },
+  {
+    "revision": "a2e8648fa5c4e79bc86266388580152b",
+    "url": "/img/1.a2e8648f.jpg"
+  },
+  {
+    "revision": "c20972ccb788c51b720919c234379451",
+    "url": "/img/5.c20972cc.jpg"
+  },
+  {
+    "revision": "e52e74c460cba9e93889dfb8e42acc94",
+    "url": "/img/joke.e52e74c4.png"
+  },
+  {
+    "revision": "f8d25855c0138f5354179d260b3550c7",
+    "url": "/img/homepage-logo.f8d25855.png"
+  },
+  {
+    "revision": "e07ac5de68b512a83781ed3ff85380e9",
+    "url": "/img/devil.e07ac5de.png"
+  },
+  {
+    "revision": "b1c001a70eafb9c8e8888124c2a8eef8",
+    "url": "/img/1.b1c001a7.jpg"
+  },
+  {
+    "revision": "890c6d842f71c4130f4e657e70a0661f",
+    "url": "/img/2.890c6d84.jpg"
+  },
+  {
+    "revision": "8c38c5ec965997ee469de75d7e3d4e13",
+    "url": "/img/2.8c38c5ec.jpg"
+  },
+  {
+    "revision": "39b5339041d3d3a8078ec671fe472a07",
+    "url": "/img/galaxy.39b53390.png"
+  },
+  {
+    "revision": "35c97fd073039ba0949b6214568b9529",
+    "url": "/img/3.35c97fd0.jpg"
+  },
+  {
+    "revision": "5b135a2be4af6c8f3f886909cf94b0f5",
+    "url": "/img/4.5b135a2b.jpg"
+  },
+  {
+    "revision": "211e61a875b2b4b4dc50562cfbf0508c",
+    "url": "/img/leaf.211e61a8.png"
+  },
+  {
+    "revision": "14f319aa830dea1ab7870512376b23d4",
+    "url": "/img/4.14f319aa.jpg"
+  },
+  {
+    "revision": "c2d892576fd2a3760d7b1dfcf271e222",
+    "url": "/img/1.c2d89257.jpg"
+  },
+  {
+    "revision": "e161509161659d355f45b48e9d890707",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d482b3f86bf4ef560d2d",
+    "url": "/css/app.8f86a026.css"
+  }
+];
